64a9d3e6065cbcc082ffe36cb0375bbe5cf0713a
angelorenz70 <angelorenz70@gmail.com>
2024-10-03 12:31:21 +0800

    camera test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
-from flask import Flask
+from flask import Flask, render_template
 
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    return 'Hello World'
+    return render_template('index.html')
 
 
 @app.route('/params/<param1>/<param2>', methods=['GET'])
